889a4ddde14ebd2ef866b46dc1a2384f73f80c09
Matsui Kohei <xingpingsongjing0521@gmail.com>
2024-06-12 10:02:29 +0900

    中級編DAY31　JDBCを使っていたところをMyBatisに変更

---
+++ src/main/java/plugin/enemydown/Main.java
@@ -0,0 +1,20 @@
+package plugin.enemydown;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import plugin.enemydown.command.EnemyDownCommand;
+import plugin.enemydown.command.EnemySpawnCommand;
+
+public final class Main extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    EnemyDownCommand enemyDownCommand = new EnemyDownCommand(this);
+    Bukkit.getPluginManager().registerEvents(enemyDownCommand, this);
+    getCommand("enemyDown").setExecutor(enemyDownCommand);
+    EnemySpawnCommand enemySpawnCommand = new EnemySpawnCommand();
+    getCommand("enemySpawn").setExecutor(enemySpawnCommand);
+
+  }
+
+}
